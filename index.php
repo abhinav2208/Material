50490172b78eb07b132d5a76494a3b7679893565
Abhinav Kumar <av9abby@gmail.com>
2018-03-13 13:00:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,52 +5,25 @@
 
     // Test to see the excel data stored in $sheets property
     echo '<pre>';
-    var_export($excel->sheets);
-    /*echo '<table border = "1">';
-    echo '<th>'; echo "S.No."; echo '</th>';
-    echo '<th>'; echo "Question"; echo '</th>';
+    //var_export($excel->sheets);
+    echo '<table border = "1">';
+    echo '<th>'; echo "Q.Id."; echo '</th>';
+    echo '<th>'; echo "Questions"; echo '</th>';
     echo '<th>'; echo "Answers"; echo '</th>';
-    for($i=2;$i<=697;$i++)
+    for($i=1;$i<=183;$i++)
     {
         echo '<tr>';
         echo '<td>';
-            echo($i);
+            echo(str_replace("<", "&lt;", $excel->sheets[0]['cells'][$i][1]));
         echo '</td>';
         echo '<td>';
-            echo(str_replace("<", "&lt;", $excel->sheets[0]['cells'][$i][6]));
+            echo(str_replace("<", "&lt;", $excel->sheets[0]['cells'][$i][2]));
         echo '</td>';
-        if((float)$excel->sheets[0]['cells'][$i][13]>0)
-        {
-            echo '<td>';
-            echo(str_replace("<", "&lt;", $excel->sheets[0]['cells'][$i][8]));
-            echo '</td>';
-        }
-        if((float)$excel->sheets[0]['cells'][$i][14]>0)
-        {
-            echo '<td>';
-            echo(str_replace("<", "&lt;", $excel->sheets[0]['cells'][$i][9]));
-            echo '</td>';
-        }
-        if((float)$excel->sheets[0]['cells'][$i][15]>0)
-        {
-            echo '<td>';
-            echo(str_replace("<", "&lt;", $excel->sheets[0]['cells'][$i][10]));
-            echo '</td>';
-        }
-        if((float)$excel->sheets[0]['cells'][$i][16]>0)
-        {
-            echo '<td>';
-            echo(str_replace("<", "&lt;", $excel->sheets[0]['cells'][$i][11]));
-            echo '</td>';
-        }
-        if((float)$excel->sheets[0]['cells'][$i][17]>0)
-        {
-            echo '<td>';
-            echo(str_replace("<", "&lt;", $excel->sheets[0]['cells'][$i][12]));
-            echo '</td>';
-        }
+        echo '<td>';
+            echo(str_replace("<", "&lt;", $excel->sheets[0]['cells'][$i][3]));
+        echo '</td>';
         echo '</tr>';
     }
-   echo '</table>';*/
+   echo '</table>';
    echo '</pre>';
 ?>
