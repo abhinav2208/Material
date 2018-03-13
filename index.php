b52ec0063ef455de02a7f6c77db41aa8190630c8
Abhinav Kumar <av9abby@gmail.com>
2018-03-13 12:55:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     include 'excel_reader.php';       // include the class
     $excel = new PhpExcelReader;      // creates object instance of the class
-    $excel->read('quiz.xls');   // reads and stores the excel file data
+    $excel->read('correct.xls');   // reads and stores the excel file data
 
     // Test to see the excel data stored in $sheets property
     echo '<pre>';
